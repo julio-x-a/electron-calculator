d09ba3b972e21d629227089c6d2adadfa97a40be
Julio <jrab997@gmail.com>
2023-04-30 14:32:18 -0500

    first commit 😎

---
+++ preload.js
@@ -0,0 +1,3 @@
+window.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('input').placeholder = process.platform;
+});
---
+++ renderer.js
@@ -0,0 +1,5 @@
+// For any interactions with your web contents, you want to add scripts to your renderer process. 
+// Because the renderer runs in a normal web environment, you can add a <script> tag right before 
+// your index.html file's closing </body> tag to include any arbitrary scripts you want:
+
+//code...
